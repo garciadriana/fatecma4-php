aefedeb3d95e8f2769426d64d8fca9cc5ea943b1
Adriana Garcia <adrianam.garcia@hotmail.com>
2017-11-20 15:26:12 -0200

    Correção EXE07

--- Exe07.php
+++ Exe07.php
@@ -17,27 +17,4 @@ if(isset($_POST["submit"])){
   echo "Fazendo " . $depositos . " mensais de R$${dep} <br>"
   ." a uma taxa de jurus mensal de " . number_format($taxa/$qtd ,3) . "%<br>"
   ."você tera guardado um total de R$" . number_format($total ,2);
-}else {?>
-  <!DOCTYPE html>
-  <html>
-  <head>
-    <meta charset="utf-8">
-    <title>Exe07</title>
-  </head>
-  <body>
-    <h1>Exercicio 5 - equipes com até 4 alunos</h1>
-    <p>Aluno 1 - Aluno2</p>
-    <hr>
-    <form name="fomr3" action="Exe07.php" method="POST">
-
-      <p>Quantidade de alunos na equipe </p>
-      <input type="text" name="depositos" value="">
-      <input type="text" name="qtdDep" value="">
-      <p>Nome do representante da equipe</p>
-      <input type="text" name="anos" value="1">
-      <input type="text" name="taxa" value="2">
-      <input type="submit" name="submit" value="Enviar">
-    </form>
-  </body>
-  </html>
-<?php } ?>
+} ?>
