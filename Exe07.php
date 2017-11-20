b040b5b29d36b35cbbefcdd8e2299daa31a8e863
Adriana Garcia <adrianam.garcia@hotmail.com>
2017-11-20 15:38:18 -0200

    Correção HTML do EXE07

--- Exe07.php
+++ Exe07.php
@@ -1,3 +1,21 @@
+ <!DOCTYPE html>
+  <html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercício 07</title>
+	<style>
+	body{
+		background: #008000;
+		color: #9ACD32;
+		font-family: Century Gothic;
+		
+	}
+	</style>
+  </head>
+  <body>
+  <center>
+    <h1>Valor futuro com depósitos regulares</h1>
+	<hr>
 <?php
 if(isset($_POST["submit"])){
   $dep = $_POST["depositos"];
@@ -15,6 +33,9 @@ if(isset($_POST["submit"])){
     $contador++;
   }
   echo "Fazendo " . $depositos . " mensais de R$${dep} <br>"
-  ." a uma taxa de jurus mensal de " . number_format($taxa/$qtd ,3) . "%<br>"
-  ."você tera guardado um total de R$" . number_format($total ,2);
+  ." a uma taxa de juros mensal de " . number_format($taxa/$qtd ,3) 
+  . "%<br>"
+  ."você terá guardado um total de R$" . number_format($total ,2);
 } ?>
+  </body>
+  </center>
